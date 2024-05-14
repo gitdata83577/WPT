d2248ab08562cf5cd6083543953080ed3aa89776
kd2_83577_Mrunal <mrunalkesarkar23@gmail.com>
2024-05-14 15:57:24 +0530

    Assignment completed

---
+++ Assign04/script.js
@@ -0,0 +1,24 @@
+function DoAddition() {
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    document.getElementById("result").value = parseFloat(num1) + parseFloat(num2);
+}
+
+function DoSubstraction() {
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    document.getElementById("result").value = parseFloat(num1) - parseFloat(num2);
+}
+
+function DoMultiplaction() {
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    document.getElementById("result").value = parseFloat(num1) * parseFloat(num2);
+}
+
+function DoDivision() {
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    document.getElementById("result").value = parseFloat(num1) / parseFloat(num2);
+}
+
